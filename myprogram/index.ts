3e72de4e512354626f563c7e57d9f1b31e5bc842
Amir Hossein Mafi <amir77mafi@gmail.com>
2023-03-25 08:48:12 +0330

    second commit

--- myprogram/index.ts
+++ myprogram/index.ts
@@ -1,8 +1,22 @@
 import { mySchema } from 'mylib';
 import { z } from 'zod';
 
+console.log(mySchema instanceof z.Schema); // false
+
 try {
   mySchema.parse('');
 } catch (error) {
-  console.log(error instanceof z.ZodError);
+  console.log(error instanceof z.ZodError); // false
+}
+
+// -----------------------------
+
+const localSchema = z.object({});
+
+console.log(localSchema instanceof z.Schema); // true
+
+try {
+  localSchema.parse('');
+} catch (error) {
+  console.log(error instanceof z.ZodError); // true
 }
